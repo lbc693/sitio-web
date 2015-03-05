c9793db199c05d53958e653a3214620383e7207b
Luis Barón <lbc693@hotmail.com>
2015-03-05 20:51:53 +0100

    Esqueleto y navbar marca la página que se está viendo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,39 @@
-/* jshint devel:true */
+'use strict';
 console.log('\'Allo \'Allo!');
+var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
+
+animateApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'page-home.html',
+            controller: 'mainController'
+        })
+        .when('/about', {
+            templateUrl: 'page-about.html',
+            controller: 'aboutController'
+        })
+        .when('/contact', {
+            templateUrl: 'page-contact.html',
+            controller: 'contactController'
+        });
+
+});
+
+animateApp.controller('mainController', function($scope) {
+    $scope.pageClass = 'page-home';
+});
+
+animateApp.controller('aboutController', function($scope) {
+    $scope.pageClass = 'page-about';
+});
+
+animateApp.controller('contactController', function($scope) {
+    $scope.pageClass = 'page-contact';
+});
+
+function HeaderController($scope, $location) 
+{ 
+    $scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+}
