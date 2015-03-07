93be7c2c68f46513a5221617a8ee637fb09deabb
Luis Barón <lbc693@hotmail.com>
2015-03-07 14:53:30 +0100

    Varias probatinas para conseguir que funcione el build y nada.. Quito angular :@

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,42 +1,45 @@
-'use strict';
-console.log('\'Allo \'Allo!');
-var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
+(function() {
+    'use strict';
+    var console;
+    var angular;
+    console.log('\'Allo \'Allo!');
 
-animateApp.config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'page-home.html',
-            controller: 'mainController'
-        })
-        .when('/carousel', {
-            templateUrl: 'page-home.html#carousel',
-            controller: 'mainController'
-        })
-        .when('/about', {
-            templateUrl: 'page-about.html',
-            controller: 'aboutController'
-        })
-        .when('/contact', {
-            templateUrl: 'page-contact.html',
-            controller: 'contactController'
-        });
+    function HeaderController($scope, $location) {
+        $scope.isActive = function(viewLocation) {
+            return viewLocation === $location.path();
+        };
+    }
 
-});
+    animateApp.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'page-home.html',
+                controller: 'mainController'
+            })
+            .when('/carousel', {
+                templateUrl: 'page-home.html#carousel',
+                controller: 'mainController'
+            })
+            .when('/about', {
+                templateUrl: 'page-about.html',
+                controller: 'aboutController'
+            })
+            .when('/contact', {
+                templateUrl: 'page-contact.html',
+                controller: 'contactController'
+            });
 
-animateApp.controller('mainController', function($scope) {
-    $scope.pageClass = 'page-home';
-});
+    });
 
-animateApp.controller('aboutController', function($scope) {
-    $scope.pageClass = 'page-about';
-});
+    animateApp.controller('mainController', function($scope) {
+        $scope.pageClass = 'page-home';
+    });
 
-animateApp.controller('contactController', function($scope) {
-    $scope.pageClass = 'page-contact';
-});
+    animateApp.controller('aboutController', function($scope) {
+        $scope.pageClass = 'page-about';
+    });
 
-function HeaderController($scope, $location) {
-    $scope.isActive = function(viewLocation) {
-        return viewLocation === $location.path();
-    };
-}
+    animateApp.controller('contactController', function($scope) {
+        $scope.pageClass = 'page-contact';
+    });
+}());
