c1dc1954f868273e0939c472658809f574dc7b7a
Luis Barón <lbc693@hotmail.com>
2015-03-07 21:57:44 +0100

    Funciona el mapa, pero no el build

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,25 @@
-(function() {
+/* google maps -----------------------------------------------------*/
+google.maps.event.addDomListener(window, 'load', initialize);
 
-    'use strict';
-    /* google maps -----------------------------------------------------*/
+function initialize() {
 
-    var google;
+  /* position Amsterdam */
+  var latlng = new google.maps.LatLng(52.3731, 4.8922);
 
-    function initialize() {
+  var mapOptions = {
+    center: latlng,
+    scrollWheel: false,
+    zoom: 13
+  };
+  
+  var marker = new google.maps.Marker({
+    position: latlng,
+    url: '/',
+    animation: google.maps.Animation.DROP
+  });
+  
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  marker.setMap(map);
 
-        /* position Amsterdam */
-        var latlng = new google.maps.LatLng(52.3731, 4.8922);
-
-        var mapOptions = {
-            center: latlng,
-            scrollWheel: false,
-            zoom: 13
-        };
-
-        var marker = new google.maps.Marker({
-            position: latlng,
-            url: '/',
-            animation: google.maps.Animation.DROP
-        });
-
-        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-        marker.setMap(map);
-
-    }
-    google.maps.event.addDomListener(window, 'load', initialize);
-
-}());
+};
+/* end google maps -----------------------------------------------------*/
